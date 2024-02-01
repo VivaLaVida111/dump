6b65a523b832bed8506a73f6fd660899479d9a57
VivaLaVida111 <2310745915@qq.com>
2024-02-01 09:15:02 +0800

    add login function

---
+++ src/main/java/com/example/dump/dto/UserLoginParam.java
@@ -0,0 +1,17 @@
+package com.example.dump.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * 用户登录参数
+ */
+@Getter
+@Setter
+public class UserLoginParam {
+    private String name;
+    private String password;
+
+}
+
+
